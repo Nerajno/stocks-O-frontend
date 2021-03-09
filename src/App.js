9737dd6cf883a6d1f23914b108f1acbeab9fa69f
Gerdeth <gerdethwickham@gmail.com>
2021-03-09 07:11:44 -0500

    scream

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import{Navigation,Home}from "./components";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// import{ Navigation, Home }from "./components";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
 //create your first component
 function App () {
 	
 	return (
 		<div className="App">
-			<Router>
-				<Navigation/>
-        <Switch>
-        <Route  path="/" exact component={()=> <Home/>} />
-				<Route path="/home" exact component={()=> <Home/>} />
-						{/* <Route exact path="/userprofile" component={UserProfile} />
-						<Route exact path="/mystocks" component={MyStocks} />
-						<Route exact path="/tradestocks" component={TradeStocks} />
-						<Route exact path="/leaderboard" component={LeaderBoard} />
-						<Route exact path="/transactionhistory" component={TransactionHistory} /> */}
-        </Switch>
-      </Router>
+			<Navigation/>
+			<Home/>
+
+
+		
 					
     </div>
 	);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-// export { default as Navigation } from "./Navigation";
-// export { default as Home } from "./Home";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
