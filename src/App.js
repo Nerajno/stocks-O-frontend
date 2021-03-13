ca40768685cfb50b9253da591be9861eb6041d00
Gerdeth <gerdethwickham@gmail.com>
2021-03-13 11:50:41 -0500

    test api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // import{ Navigation, Home }from "./components";
@@ -6,22 +6,53 @@ import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
+import Favorites from "./components/Favorites";
 //create your first component
+
+
+
+
+
 function App () {
 	
+
+	useEffect(()=>{
+		getStocks()
+	},[]);
+
+	
+	function getStocks(FAVORITES){
+		fetch("https://morning-star.p.rapidapi.com/market/v2/get-movers", {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-key": "607c356a15msh664378701f6febfp170069jsn3c9f418ebaf0",
+				"x-rapidapi-host": "morning-star.p.rapidapi.com"
+			}
+			
+})
+.then((res)=>res.json())
+.then(data => {
+	console.log(data);
+	
+})
+.catch(err => {
+	console.error(err);
+
+});
+	}
 	return (
 		<div className="App">
 			<Router>
 				<Navigation/>
-				<Switch>
+				<Favorites/>
+				{/* <Switch>
 					<Route path="/" exact component={()=><Home/>}/>
 					<Route path="/home" exact component={()=><Home/>}/>
 					<Route path="/login" exact component={()=><LogIn/>}/>
 					<Route path="/signup" exact component={()=><SignUp/>}/>
-				</Switch>
+				</Switch> */}
 			</Router>
     	</div>
 	);
 }
-
 export default App;
---
+++ src/components/Favorites.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Favorites=({standardName,lastPrice})=>(
+    <div>
+        <p>{standardName}</p>
+        <p>{lastPrice}</p>
+    </div>
+)
+
+export default Favorites;
