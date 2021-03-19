acfd8e1eb550df79bb3fbe769e462624376b622a
Gerdeth <gerdethwickham@gmail.com>
2021-03-19 07:46:52 -0400

    stuff

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,54 +1,57 @@
 import React,{useEffect, useState} from "react";
 
 
-function FavoritesData({ticker,standardName,lastPrice,percentChange}){
+function FavoritesData({price,change_percentage}){
     return(
-        <table className="table">
+        <div>
+            <div>Price:{price}</div>
+            <div>Percent Change:{change_percentage}</div>
+        </div>
+        // <table className="table">
                 
                 
                 
-                <tbody>
+        //         <tbody>
                     
-                    <th scope="row">{ticker}</th>
-                    <td>{standardName}</td>
-                    <td>{lastPrice}</td>
-                    <td>{percentChange}</td>
+        //             <th scope="row">TESLA</th>
                     
-                </tbody>
-        </table>
+        //             <td>{price}</td>
+        //             <td>{change_percentage}</td>
+                    
+        //         </tbody>
+        // </table>
     )
 }
 
-function Favorites({ticker,standardName,lastPrice, percentChange}){
+function Favorites({price, change_percentage}){
     
     const [stocksData, setstocksData]= useState([]);
         
 
-        // useEffect(()=>{
-        //     getStocks()
-        // },[]);
+        useEffect(()=>{
+            getStocks()
+        },[]);
 
     
-    //     const getStocks=(FAVORITES)=>{
-    //         fetch("https://morning-star.p.rapidapi.com/market/v2/get-movers", {
-    //             "method": "GET",
-    //             "headers": {
-    //                 "x-rapidapi-key": "607c356a15msh664378701f6febfp170069jsn3c9f418ebaf0",
-    //                 "x-rapidapi-host": "morning-star.p.rapidapi.com"
-    //             }
-                
-    //     })
-    //     .then((res)=>res.json())
-    //     .then(data => {
-    //         console.log(data.actives);
-    //         setstocksData(data.actives);
+        const getStocks=(FAVORITES)=>{
+            fetch("https://realstonks.p.rapidapi.com/TSLA", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "607c356a15msh664378701f6febfp170069jsn3c9f418ebaf0",
+                "x-rapidapi-host": "realstonks.p.rapidapi.com"
+            }
+        })
+        .then((res)=>res.json())
+        .then((data) => {
+            console.log(data);
+            setstocksData(data);
             
-    //     })
-    //     .catch(err => {
-    //         console.error(err);
+        })
+        .catch(err => {
+            console.error(err);
 
-    //     });
-    // }
+        });
+    }
         
 
 
@@ -56,7 +59,7 @@ function Favorites({ticker,standardName,lastPrice, percentChange}){
         
     return(
         <div>
-            <table className="table">
+            {/* <table className="table">
                 <thead>
                     <tr>
                     <th scope="col">Symbol</th>
@@ -65,9 +68,9 @@ function Favorites({ticker,standardName,lastPrice, percentChange}){
                     <th scope="col">Open P&L</th>
                     </tr>
                 </thead>
-                </table>
-            {stocksData.length > 0 &&  stocksData.map((stocks)=>
-                <FavoritesData key ={stocks.id}{...stocks}/>
+                </table> */}
+            {stocksData.length > 0 && [stocksData].map(stocks=>
+                <FavoritesData key ="{stocks}"{...stocks}/>
             )}
         </div>
 
